74fac8e55d97a48d4348688d004f33f5405c48c4
XDORAEMONX-314159 <104264783+XDORAEMONX-314159@users.noreply.github.com>
2022-08-02 21:51:02 +0800

    Update Youtube Downloader.py

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -7,23 +7,28 @@ def download():
     for stream in my_video.streams:
         print(stream)
     my_video = my_video.streams.get_highest_resolution()
-    my_video.download(output_path="write the path here")    
+    my_video.download(output_path=str(output_entry.get()))    
     
     tkinter.messagebox.showinfo(title= 'Download complete', message = "Download complete")
     
 window = tk.Tk()
 window.title("Youtube Downloader")
-window.geometry("600x600+420+150")
-window.iconphoto(True, tk.PhotoImage(file='write the photo path here'))
+window.geometry("300x160+420+150")
 
-label = tk.Label(window, text= 'Thanks for using Youtube Downloader')
-label.pack()
+label_1 = tk.Label(window, text= 'Thanks for using Youtube Downloader')
+label_1.pack()
 
-label = tk.Label(window, text= 'Please enter the URL down')
-label.pack()
+label_URL = tk.Label(window, text= 'Please enter the URL down')
+label_URL.pack()
 
 url_entry = tk.Entry(window, width=40)
 url_entry.pack()
+
+label_path = tk.Label(window, text="Please enter the output_path down")
+label_path.pack()
+
+output_entry = tk.Entry(window, width=40)
+output_entry.pack()
     
 button = tk.Button(window, text="Download Now", command=download)
 button.pack()
